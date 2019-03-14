c343b15556afc7ff78856fc99402473d1a20834c
Morgan Dorfman <mdorfman21@gmail.com>
2019-03-14 19:03:44 -0400

    added the projects to add dynamically

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,3 +1,50 @@
 $("#resume").on("click", function() {
-  window.open("./images/Morgan Dorfman Résumé.pdf");
+  window.open("/assets/images/Morgan Dorfman Résumé.pdf");
+});
+
+$("#github").on("click", function() {
+  window.open("https://github.com/mdorfman21");
+});
+
+$("#linkedin").on("click", function() {
+  window.open("https://www.linkedin.com/in/mdorfman21/");
+});
+
+var projectOne = {
+  title: "Be Your Own Bartender",
+  deployedSource: "https://mdorfman21.github.io/Group-Router/",
+  githubSource: "https://github.com/mdorfman21/Group-Router"
+};
+var projectTwo = {
+  title: "Giftastic",
+  deployedSource: "https://mdorfman21.github.io/GifTastic/",
+  githubSource: "https://github.com/mdorfman21/GifTastic"
+};
+var projectThree = {
+  title: "Inception Trivia Game",
+  deployedSource: "https://mdorfman21.github.io/TriviaGame/",
+  githubSource: "https://github.com/mdorfman21/TriviaGame"
+};
+
+var projectArray = [projectOne, projectTwo, projectThree];
+
+projectArray.forEach(project => {
+  var newDiv = $("<div>").addClass("card bg-success margin d-inline-block");
+  var title = $("<h3>").text(project.title);
+  var deployed = $("<a>")
+    .attr({
+      href: project.deployedSource,
+      target: "_blank"
+    })
+    .addClass("margin d-inline")
+    .text("Deployed Project Page");
+  var github = $("<a>")
+    .attr({
+      href: project.githubSource,
+      target: "_blank"
+    })
+    .addClass("margin d-inline")
+    .text("GitHub Code");
+  newDiv.append(title, deployed, github);
+  $("#append-to").append(newDiv);
 });
